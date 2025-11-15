272877c3eb146bbfe7b9850496f9541eb9bb3000
Jorge Núñez <94089914+jphants@users.noreply.github.com>
2025-11-15 13:28:44 -0500

    redo

--- src/components/subtitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Subtitle({ children }) {
-    return (
-        <h1 className="text-2xl font-bold text-start text-gray-800 mb-6">
-            {children}
-        </h1>
-    );
-}
